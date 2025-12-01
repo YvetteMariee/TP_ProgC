8a5261e4abc092c20fffbbcb8911f58f425e8ecf
Marie Paule Yvette <162718920+YvetteMariee@users.noreply.github.com>
2025-12-01 14:29:19 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; // tu peux tester d'autres valeurs < 10
-    int i, j;
+    int compteur = 5; // tu peux changer la valeur, mais < 10
 
-    for (i = 1; i <= compteur; i++) {
-        for (j = 1; j <= i; j++) {
-            if (i % 2 == 0 && j < i) {
-                printf("# "); // mettre # pour les lignes paires sauf dernière position
-            } else {
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+
+            // Première ligne ou dernière ligne : que des *
+            if (i == 1 || i == compteur) {
                 printf("* ");
+            } 
+            else {
+                // Lignes du milieu : alternance * #
+                if (j % 2 == 1) {
+                    printf("* ");
+                } else {
+                    printf("# ");
+                }
             }
         }
         printf("\n");
